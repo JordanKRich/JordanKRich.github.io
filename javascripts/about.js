edb3f906f9cc6ad8b8dcbb5b0ee354440f053810
Jordan <jkrich05@louisville.edu>
2023-12-06 11:04:40 -0500

    test

---
+++ javascripts/about.js
@@ -0,0 +1,16 @@
+// JavaScript for opening and closing the resume overlay
+document.addEventListener('DOMContentLoaded', function () {
+    const viewResumeButton = document.querySelector('.view-resume');
+    const overlay = document.querySelector('.overlay');
+    const resumeOverlay = document.querySelector('.resume-overlay');
+    const closeButton = document.querySelector('.close-button');
+
+    viewResumeButton.addEventListener('click', function () {
+        overlay.style.display = 'block';
+        resumeOverlay.src = 'images/Official_Resume.png';
+    });
+
+    closeButton.addEventListener('click', function () {
+        overlay.style.display = 'none';
+    });
+});
